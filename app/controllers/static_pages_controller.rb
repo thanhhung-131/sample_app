911e1de9e497c8cdeb3effeaaa9605a34522afa4
Ho Thanh Hung <hothanhhung131@gmail.com>
2024-02-27 17:56:29 +0700

    chapter 13

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,16 @@
 
 # StaticPagesController is a controller for static pages.
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    return unless logged_in?
+
+    @micropost = current_user.microposts.build
+    @pagy, @feed_items = pagy(current_user.feed.newest, items: Settings.page_10)
+  end
 
   def help; end
+
+  def about; end
+
+  def contact; end
 end
